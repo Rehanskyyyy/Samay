320160dad7437a8629a572d38b96abb0dd252c0f
Rehanskyyyy <moh.rehan5292@gmail.com>
2025-01-13 12:12:38 +0530

    Change the clock format to 24 hour

--- script.js
+++ script.js
@@ -26,11 +26,6 @@ function updateClock() {
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
-
-    if (hours === 13) {
-        hours = 1;
-    }
-
     if (hours < 10) {
         hours = "0" + hours;
     }
